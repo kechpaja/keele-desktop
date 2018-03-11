0fa39ccffdd5cad1eae094a67c2c88046d058b2b
Kelvin Jackson <kechpaja@comcast.net>
2018-03-11 15:22:31 +0200

    Making a bit more progress on grammar and vocab pages

--- src/pages/GrammarPage.java
+++ src/pages/GrammarPage.java
@@ -8,5 +8,11 @@ public class GrammarPage extends ActivityPanel {
      * Added to keep compiler happy
      */
     private static final long serialVersionUID = 3301859358004448722L;
+    
+    
+    // TODO figure out how this corresponds to what's stored in the lesson and thus in JSON. 
+    
+    
+    // TODO allow for tables and stuff. 
 
 }
--- src/pages/VocabPage.java
+++ src/pages/VocabPage.java
@@ -1,5 +1,8 @@
 package pages;
 
+import java.util.List;
+
+import lessons.TranslatedPair;
 import main.ActivityPanel;
 
 public class VocabPage extends ActivityPanel {
@@ -8,5 +11,21 @@ public class VocabPage extends ActivityPanel {
      * Added to keep compiler happy
      */
     private static final long serialVersionUID = -2475680546095874083L;
+    
+    public VocabPage(List<TranslatedPair> vocab) {
+        // TODO layout manager
+        
+        for (TranslatedPair pair : vocab) {
+            // TODO create horizontal container and add all items. Then, add container to this. 
+            
+            // TODO we can also play around with group layouts. Maybe later. 
+        }
+        
+        this.revalidate();
+    }
+    
+    // TODO get list of translated pairs from lesson, and display word, images, sounds files, translations for each entry
+    
+    // TODO allow non-mouseover translations and images to be toggled
 
 }
